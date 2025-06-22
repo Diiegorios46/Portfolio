803a0991407bd78eeed4b03cc27e63969c452e17
Diiegorios46 <diegoavilaaa0@gmail.com.com>
2025-06-22 18:10:05 -0300

    Refactor project section layout and enhance skill display with improved styling and functionality

--- javascript/links.js
+++ javascript/links.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-document.querySelectorAll('.project').forEach(div => {
+document.querySelectorAll('.img-link').forEach(div => {
     div.addEventListener('click', () => {
         const url = div.getAttribute('data-link');
         window.open(url, '_blank');
